92a125e7a9db06188c4f85962c4bbfc1f4e98d70
devmentor.pl <55095257+devmentor-pl@users.noreply.github.com>
2021-03-11 18:18:25 +0100

    update import path of context

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 // ./src/components/App.js
 import React from 'react';
 import Box from './Box';
-import { TextContext } from './../context/';
+import { TextContext } from '../context';
 
 export default class App extends React.Component {
     render() {
